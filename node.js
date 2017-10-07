eaae70dd745732d96b615ae92614b7304055b6f6
yucong02 <yucong02@baidu.com>
2017-10-07 20:46:44 +0800

    Added Deep

--- node.js
+++ node.js
@@ -2,24 +2,44 @@ var fs = require('fs');
 var nps = require('path');
 
 exports.markdownTransfomer = function (opt, gift, require) {
+	var prefix = opt.prefix || '@'
+	var suffix = opt.suffix || '@'
+	var deep = ('deep' in opt) ? !!opt.deep : true;
+
 	if (!gift.data) return gift.data;
 	var filename = gift.filename;
-	console.log(filename);
-	var dirname = nps.dirname(filename)
-	gift.data = gift.data.replace(
-		/@\s*(.+?)\s*@/g,
-		function (m, path) {
-			path = path.trim();
-			if (!path) return m;
-			path = nps.join(dirname, path);
-			try {
-				path = require.resolve(path);
-			} catch (err) {
-				return m;
+	// console.log(filename);
+
+	function replace(content, filename) {
+		var dirname = nps.dirname(filename);
+
+		return content.replace(
+			new RegExp('^\\s*' + prefix + '\\s*(.+?)\\s*' + suffix + '\\s*(\\n?)$', 'gm'),
+			// /@\s*(.+?)\s*@/g,
+			function (m, path, newlineOrNull) {
+				path = path.trim();
+				if (!path) return m;
+				path = nps.join(dirname, path);
+				try {
+					path = require.resolve(path);
+				} catch (err) {
+					return m;
+				}
+				if (path === filename) {
+					return m;
+				}
+				
+				var fileContent = fs.readFileSync(path).toString();
+				// console.log(fileContent);
+				if (deep) {
+					fileContent = replace(fileContent, path);
+				}
+				
+				return fileContent + newlineOrNull;
 			}
-			console.log(path);
-			return fs.readFileSync(path).toString();
-		}
-	)
+		);
+	}
+
+	gift.data = replace(gift.data, filename);
 	return gift.data;
 };
