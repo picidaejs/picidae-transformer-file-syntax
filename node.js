ec02b7b3cb4e56bb6eac0b99c935cb8bb9a14a7b
yucong02 <yucong02@baidu.com>
2017-10-08 19:25:54 +0800

    added 'link::[title]path'

--- node.js
+++ node.js
@@ -7,46 +7,78 @@ exports.markdownTransfomer = function (opt, gift, require) {
 	var suffix = opt.suffix || '@'
 	var deep = ('deep' in opt) ? !!opt.deep : true;
 
+	var filesMap = gift.filesMap;
+	var path = gift.path;
+	var publicPath = gift.publicPath;
 	if (!gift.data) return gift.data;
-	var filename = gift.filename;
+	var filename = filesMap[path];
 	// console.log(filename);
 
 	function replace(content, filename) {
 		var dirname = nps.dirname(filename);
 
+		function checkPath(path) {
+			path = path.trim();
+			if (!path) return false;
+			path = nps.join(dirname, path);
+			try {
+				path = require.resolve(path);
+			} catch (err) {
+				return false;
+			}
+			if (path === filename) {
+				return false;
+			}
+			return path;
+		}
+
 		return content.replace(
-			new RegExp('^\\s*' + prefix + '\\s*(.+?)\\s*' + suffix + '\\s*(\\n?)$', 'gm'),
+			new RegExp('(^\\s*)' + prefix + '\\s*(.+?)\\s*' + suffix + '(\\s*\\n?)$', 'gm'),
 			// /@\s*(.+?)\s*@/g,
-			function (m, path, newlineOrNull) {
-				// var lastIndexOfQ = m.lastIndexOf('?');
-				// var pathOption = {}
-				// if (lastIndexOfQ >= 0) {
-				// 	var query = m.substr(lastIndexOfQ)
-				// 	pathOption = loaderUtil.parseQuery(query);
-				// }
-				//
-				// if ('link' in pathOption) {
-				//
-				// }
-				path = path.trim();
-				if (!path) return m;
-				path = nps.join(dirname, path);
-				try {
-					path = require.resolve(path);
-				} catch (err) {
-					return m;
-				}
-				if (path === filename) {
-					return m;
+			function (m, preappend, path, newlineOrNull) {
+				var currLink = path.trim();
+				var fullpath = '';
+
+				// link::[why-need-require]../refs/why-need-require.md
+				if (currLink.startsWith('link::')) {
+					var title = '';
+					path = '';
+
+					if (/^link::\[(.+?)\](.+)/.test(currLink)) {
+						title = RegExp.$1;
+						path = RegExp.$2;
+						title = title && title.trim() ? title.trim() : path;
+					} else if (/^link::(.+)/.test(currLink)) {
+						title = RegExp.$1;
+						path = RegExp.$1;
+					}
+
+					// console.log(title, path);
+					fullpath = checkPath(path);
+					// console.log(fullpath);
+
+					if (fullpath) {
+						var link = Object.keys(filesMap).find(function (link) {
+							return filesMap[link] === fullpath;
+						});
+						if (link) {
+							link = link === 'INDEX' ? '' : link;
+							return preappend + '[' + title + '](' + publicPath.replace(/\/*$/, '/') + link.replace(/^\/+/, '') + ')' + newlineOrNull;
+						}
+					}
+
 				}
 
-				var fileContent = fs.readFileSync(path).toString();
-				// console.log(fileContent);
-				if (deep) {
-					fileContent = replace(fileContent, path);
-				}
+				fullpath = checkPath(currLink);
+				if (fullpath) {
+					var fileContent = fs.readFileSync(fullpath).toString();
+					if (deep) {
+						fileContent = replace(fileContent, fullpath);
+					}
 
-				return fileContent + newlineOrNull;
+					return fileContent + newlineOrNull;
+				}
+				return m;
 			}
 		);
 	}
